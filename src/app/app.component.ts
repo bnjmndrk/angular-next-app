6b90d9a62187b3d3c27a258e6cc80380affd0044
Benjamin Dark <benjamin.dark@leidos.com>
2018-11-14 21:01:21 -0500

    comments & explanations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',      // unlike css styles, only 1 template can be listed here
+  // styleUrls: ['./app.component.css']    // this is an array which allows for pointing to multiple stylesheets
+  styles: [`                               
+  h3 {
+    color:dodgerblue;
+  }
+  `]                                      // example of inline style array with backtics for multiple strings 
 })
 export class AppComponent {
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+//  templateUrl: './servers.component.html',                        // example of using html template
+//  template: '<app-server></app-server><app-server></app-server>', // example of using in-line templating
+  template: `
+          <app-server></app-server>
+          <app-server></app-server>`,                               // example of using in-line templating with back-tics for multi-line entry
+
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
