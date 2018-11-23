91ed4a7595ed0c4cf3183a1d7e8a676073d8f701
Benjamin Dark <benjamin.dark@leidos.com>
2018-11-22 22:58:02 -0500

    Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+/*
+Components have 3 parts:
+  1) a selector - this is the html tag name which will represent the logic
+  2) where the file which contains the html for this component is located
+  3) the ccs styles to apply to this component
+*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',      // unlike css styles, only 1 template can be listed here
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+/*
+The selector can be written as:
+  1) a selector name (e.g. 'app-servers') -> use the <app-servers> as html tags -or-
+  2) as an element attribute (e.g. '[app-servers]') -> use app-servers inside an html tag as an attribute
+  3) as a class (e.g. '.app-servers') -> use app-servers class inside an html tag
+*/
 @Component({
-  selector: 'app-servers',
-//  templateUrl: './servers.component.html',                        // example of using html template
-//  template: '<app-server></app-server><app-server></app-server>', // example of using in-line templating
-  template: `
-          <app-server></app-server>
-          <app-server></app-server>`,                               // example of using in-line templating with back-tics for multi-line entry
+  selector: '.app-servers',                   // as a class
+//  selector: '[app-servers]',                // as an element attribute
+//  selector: 'app-servers',                  // as an html tag - most common
+  templateUrl: './servers.component.html',                        // example of using html template
+  // template: `
+  //         <app-server></app-server>
+  //         <app-server></app-server>`,                               // example of using in-line templating with back-tics for multi-line entry
 
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer:boolean = false;
+
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
